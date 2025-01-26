aacfe2fc78ee2cbfe3179f0602983a1e6646b317
devopsdeepaks <deepaksinghalgpg@gmail.com>
2025-01-26 03:40:36 -0800

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <h1>
+      Hello World!!</h1>
+      <Button>Submit</Button>
+    </div>
+    );
+}
