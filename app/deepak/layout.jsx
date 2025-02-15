922af972fbc621128adf22715702b26a6f5d6b06
devopsdeepaks <deepaksinghalgpg@gmail.com>
2025-02-15 16:58:14 +0530

    error solve

--- app/deepak/layout.jsx
+++ app/deepak/layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from './_component/Header'
 
 const DeepakLayout = ({ children }) => {
     return (
@@ -7,7 +6,6 @@ const DeepakLayout = ({ children }) => {
             {/* <div className='dark bg-[#0c0c0d] h-screen w-screen' > */}
             <div className='dark bg-black bg-grid-white/[0.1] h-screen w-screen' >
 
-                <Header />
                 {children} {/* This will render only the specific page content */}
             </div>
         </div>
--- app/deepak/page.jsx
+++ app/deepak/page.jsx
@@ -1,21 +1,8 @@
-import { TextGenerateEffect } from '@/components/ui/text-generate-effect'
 import React from 'react'
-import { SparklesPreview } from '../_component/SparklesPreview'
-import { AddContainer } from './_component/AddContainer'
-import { TimelineDemo } from './_component/Web'
 
 const DeepakPage = () => {
     return (
-        <div className='text-white pt-32 p-8 flex flex-col '>
-            <TextGenerateEffect
-                className={'text-4xl w-full text-center sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 '}
-                words="RoadMaps" />
-            {/* <SparklesPreview /> */}
-            <div className='flex-row'>
-                <AddContainer />
-                <TimelineDemo />
-            </div>
-        </div>
+        <div>DeepakPage</div>
     )
 }
 
